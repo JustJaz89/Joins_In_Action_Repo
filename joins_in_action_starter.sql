1055a5bb5eed0075cbc7d7137f315423e8c6958b
Jazmin Ivy <princessivy07@yahoo.com>
2023-03-08 11:37:15 -0600

    Completed Problem 3 in joins_in_action_starter query

--- joins_in_action_starter.sql
+++ joins_in_action_starter.sql
@@ -24,6 +24,7 @@ SELECT * FROM movies RIGHT JOIN users on movie_id = favorite_movie_id;
 -- Do a inner join to get only the rows from the movies tables that have matching rows in the users tables.
 -- EXPECTED RESULT: 14 Rows Returned
 
+SELECT * FROM movies INNER JOIN users on movie_id = favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which lead studio's movies are favorited by users the most.
